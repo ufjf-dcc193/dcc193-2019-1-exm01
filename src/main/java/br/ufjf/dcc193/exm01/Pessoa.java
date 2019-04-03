39562be8bb77e05534c00dcafe7f230886ef2b54
Igor Knop <igorknop@gmail.com>
2019-04-03 19:20:05 -0300

    Remove javadoc

--- src/main/java/br/ufjf/dcc193/exm01/Pessoa.java
+++ src/main/java/br/ufjf/dcc193/exm01/Pessoa.java
@@ -8,26 +8,15 @@ public class Pessoa {
         this.nome = "";
         this.idade = 0; 
     }
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
-    }/**
-     * @return the nome
-     */
+    }
     public String getNome() {
         return nome;
     }
-    /**
-     * @param idade the idade to set
-     */
     public void setIdade(Integer idade) {
         this.idade = idade;
     }
-    /**
-     * @return the idade
-     */
     public Integer getIdade() {
         return idade;
     }
