146bc007553a34e9203506ad8d1f9e903716f9ea
Igor Knop <igorknop@gmail.com>
2019-04-03 20:07:24 -0300

    Adiciona lista de pessoas

--- src/main/java/br/ufjf/dcc193/exm01/HomeController.java
+++ src/main/java/br/ufjf/dcc193/exm01/HomeController.java
@@ -1,13 +1,18 @@
 package br.ufjf.dcc193.exm01;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+    @Autowired
+    PessoaRepository repPessoa;
 
     @RequestMapping("index.html")
     ModelAndView home(Pessoa p){
@@ -19,5 +24,14 @@ public class HomeController {
     @RequestMapping("form.html")
     String form(){
         return "form";
+    }
+    
+    @RequestMapping("pessoas.html")
+    ModelAndView pessoas() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("pessoa-list");
+        List<Pessoa> pessoas = repPessoa.findAll();
+        mv.addObject("galera", pessoas);
+        return mv;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/pessoa-list.jsp
@@ -0,0 +1,15 @@
+<%@page pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>EXM01: Lista de Pessoas</title>
+</head>
+
+<body>
+    <h1>EXM01: Lista de Pessoas</h1>
+    <p>${galera}</p>
+</body>
+
+</html>
