7625a6bf0495a5b8b338073144f36efeee7eb79f
Igor Knop <igorknop@gmail.com>
2019-04-17 19:30:23 -0300

    Altera persisitência

--- src/main/java/br/ufjf/dcc193/exm01/DemoApplication.java
+++ src/main/java/br/ufjf/dcc193/exm01/DemoApplication.java
@@ -9,12 +9,14 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+		/*
 		PessoaRepository rep = ctx.getBean(PessoaRepository.class);
 		Pessoa p1 = new Pessoa("Fulano", 23);
 		rep.save(p1);
 		rep.save(new Pessoa("Beltrano", 17));
 		rep.save(new Pessoa("Ciclano", 37));
 		System.out.print(rep.findAll().toString());
+		*/
 	}
 
 }
